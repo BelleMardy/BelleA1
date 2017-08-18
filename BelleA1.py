889077b747e38eb0ab700f5e649f2e5520e45a8a
Belle <belle.belle@my.jcu.edu.au>
2017-08-18 20:43:54 +1000

    print welcome line

--- BelleA1.py
+++ BelleA1.py
@@ -1,1 +1,8 @@
-#Assessment 1
+#  Assessment 1
+#  Title, artist, year, whether it is required (y) or learned (n)
+#  User can choose to the list of songs, sorted by artist then by title
+#  User can add new songs and marked songs as learned
+#  Cannot change songs from learned to required
+
+title = "Songs to Learn 1.0 - by Belle"
+print (title)
